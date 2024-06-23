7dd59540874f959386c830fb3ea0d1867efab07b
PhamTuanAnh20225542 <anh.pt225542@sis.hust.edu.vn>
2024-06-23 23:26:08 +0700

    add new function

--- Web-log-analysis/src/main/java/hust/soict/cybersec/webloganalysis/util/Config.java
+++ Web-log-analysis/src/main/java/hust/soict/cybersec/webloganalysis/util/Config.java
@@ -3,23 +3,26 @@ package hust.soict.cybersec.webloganalysis.util;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import javafx.scene.control.ComboBox;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Config {
     public static String apacheLogPath;
     public static String modsecLogPath;
+    public static String currentPath;
 
     public static void createNewProfile(JsonObject profile) {
         apacheLogPath = profile.get("apacheLogPath").getAsString();
         modsecLogPath = profile.get("modsecLogPath").getAsString();
-
-        String filePath = "src/main/resources/hust/soict/cybersec/webloganalysis/profile.txt";
-        try (FileWriter writer = new FileWriter(filePath, true)){
+        
+        try (FileWriter writer = new FileWriter(currrentPath + File.separator + "profile.txt", true)){
             writer.write(profile.toString() + "\n");
         }
         catch (IOException e) {
@@ -29,7 +32,7 @@ public class Config {
     }
 
     public static void loadProfile(ComboBox<String> profileCb) {
-        String filePath = "src/main/resources/hust/soict/cybersec/webloganalysis/profile.txt";
+        String filePath = currrentPath + File.separator + "profile.txt"
         Gson gson = new Gson();
         try (FileReader reader = new FileReader(filePath);
              BufferedReader br = new BufferedReader(reader)) {
@@ -45,7 +48,7 @@ public class Config {
     }
 
     public static boolean checkProfile(String pass, String name) {
-        String filePath = "src/main/resources/hust/soict/cybersec/webloganalysis/profile.txt";
+        String filePath = currrentPath + File.separator + "profile.txt"
         Gson gson = new Gson();
         try (FileReader reader = new FileReader(filePath);
              BufferedReader br = new BufferedReader(reader)) {
@@ -65,4 +68,55 @@ public class Config {
         return false;
     }
 
+    public static void logInAsVisitor() {
+        String accessFile = currrentPath + File.separator + "access.log";
+        String modsecFile = currrentPath + File.separator + "modsec.log";
+        apacheLogPath = accessFile;
+        modsecLogPath = modsecFile;
+        Path accessPath = Paths.get(accessFile);
+        Path modsecPath = Paths.get(modsecFile);
+        if (!Files.exists(accessPath) || !Files.exists(modsecPath)) {
+            try {
+                Path newAccessFile = Files.createFile(accessPath);
+                Path newModsecFile = Files.createFile(modsecPath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            return;
+        }
+
+        InputStream inputStream = Main.class.getResourceAsStream("log_sample/access.log");
+        try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(accessFile)));
+            String line;
+            while ((line = br.readLine()) != null) {
+                bw.write(line);
+                bw.newLine();
+            }
+            bw.flush();
+            bw.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        InputStream inputStream2 = Main.class.getResourceAsStream("log_sample/modsec.log");
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream2));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(modsecFile)));
+            String line;
+            while ((line = br.readLine()) != null) {
+                bw.write(line);
+                bw.newLine();
+            }
+            bw.flush();
+            bw.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
